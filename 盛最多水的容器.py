5052aabfb28d3b53736669b10c1d5bd309a3e386
duskkk <249106499@qq.com>
2019-11-20 12:52:58 +0800

    new solution

--- 盛最多水的容器.py
+++ 盛最多水的容器.py
@@ -14,3 +14,14 @@
 输入: [1,8,6,2,5,4,8,3,7]
 输出: 49
 """
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i,j=0,len(height)-1
+        cur = 0
+        while(i<j):
+            cur = max(cur,min(height[i],height[j])*(j-i))
+            if(height[i]<height[j]):
+                i+=1
+            else:
+                j-=1
+        return cur
